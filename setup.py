62e2c45f0c30b3c500c589f86e2424d6c4fd1bdc
colorfulfool <t0ny.hamster@gmail.com>
2013-09-03 14:53:10 +0300

    Fixed setup.py glitch

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
  
-long_description = open('README.txt').read()
+long_description = open('README.md').read()
  
 setup(
     name='django-taggit-autocomplete',
